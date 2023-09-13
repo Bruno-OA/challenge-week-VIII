1eca8fc9d514034c605c40a49ffd00e989afd986
Bruno-OA <brunooliveiraalves78@gmail.com>
2023-09-13 20:16:22 -0300

    feat: creating PostMapping User

--- auth-and-auto/src/main/java/br/com/ms/authandauto/model/User.java
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/model/User.java
@@ -1,26 +1,36 @@
 package br.com.ms.authandauto.model;
 
+import br.com.ms.authandauto.dto.UserRequestDTO;
 import jakarta.persistence.*;
-import lombok.Data;
-import javax.validation.constraints.*;
+import lombok.*;
 
-@Table(name = "USERS")
+@Table(name = "users")
 @Entity(name = "users")
-@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+//    @NotNull
     private String name;
 
-    @Email(message = "Email inválido")
+//    @Email(message = "Email inválido")
     private String email;
 
-    @NotEmpty(message = "A senha deve ser informada")
-    @Size(min = 6, message = "A senha deve ter no minimo 6 caracteres")
+//    @NotEmpty(message = "A senha deve ser informada")
+//    @Size(min = 6, message = "A senha deve ter no minimo 6 caracteres")
     private String password;
     private boolean admin;
+
+    public User(UserRequestDTO data){
+        this.name = data.name();
+        this.email = data.email();
+        this.password = data.password();
+        this.admin = data.admin();
+    }
 
 }
