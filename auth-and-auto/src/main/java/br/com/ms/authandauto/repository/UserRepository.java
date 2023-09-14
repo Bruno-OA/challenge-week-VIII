7e2c06f8abe5c27f6ad76e339c1a7cbd8aa647a7
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-14 13:47:37 -0300

    build: adding new dependencies

--- auth-and-auto/src/main/java/br/com/ms/authandauto/repository/UserRepository.java
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/repository/UserRepository.java
@@ -2,7 +2,9 @@ package br.com.ms.authandauto.repository;
 
 import br.com.ms.authandauto.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
