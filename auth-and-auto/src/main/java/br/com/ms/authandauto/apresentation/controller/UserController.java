849fa1fa8c9a054873e614001f6571b1f2085088
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-15 13:15:58 -0300

    feat: Creating all controllers

---
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/apresentation/controller/UserController.java
@@ -0,0 +1,49 @@
+package br.com.ms.authandauto.apresentation.controller;
+
+import br.com.ms.authandauto.application.dtos.UserDTO;
+import br.com.ms.authandauto.application.interfaces.IMicroserviceService;
+import br.com.ms.authandauto.application.interfaces.IUserService;
+import br.com.ms.authandauto.domain.model.user.Reponse.UserMicroserviceResponse;
+import br.com.ms.authandauto.domain.model.user.Requests.UserMicroserviceRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("users")
+public class UserController {
+
+    private final IUserService _userService;
+    @Autowired
+    public UserController(IUserService userService) {
+        _userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserMicroserviceResponse>> getUsersAndPermissions() {
+        List<UserMicroserviceResponse> usersAndPermissions = _userService.getUsersAndPermissions();
+        return ResponseEntity.ok(usersAndPermissions);
+    }
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO)  {
+        UserDTO user = _userService.createUser(userDTO);
+        return ResponseEntity.ok(user);
+    }
+    @PutMapping(value = "/{id}")
+    public void updateUser(@RequestBody UserDTO userDTO, @PathVariable Long id) {
+        _userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/{userId}/microsservice/{microserviceId}")
+    public ResponseEntity<Void> bindUserToMicroservice(
+            @PathVariable Long userId,
+            @PathVariable Long microserviceId,
+            @RequestBody UserMicroserviceRequest request
+    ) {
+        _userService.bindUserToMicroservice(userId, microserviceId, request);
+        return ResponseEntity.ok().build();
+    }
+
+}
