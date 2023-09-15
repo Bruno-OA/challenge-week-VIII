fc451bd13f88f77f405ec3c4c0124d3a696cf385
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-15 13:00:55 -0300

    feat: creating the createUser method in UserService

--- auth-and-auto/src/main/java/br/com/ms/authandauto/application/service/UserService.java
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/application/service/UserService.java
@@ -2,6 +2,11 @@ package br.com.ms.authandauto.application.service;
 
 import br.com.ms.authandauto.application.dtos.UserDTO;
 import br.com.ms.authandauto.application.interfaces.IUserService;
+import br.com.ms.authandauto.domain.interfaces.IMicroserviceRepository;
+import br.com.ms.authandauto.domain.interfaces.IUserRepository;
+import br.com.ms.authandauto.domain.model.user.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,11 +14,18 @@ import java.util.List;
 
 @Service
 public class UserService implements IUserService {
-
+    private final IUserRepository _userRepository;
+    private final ModelMapper _modelMapper;
+    @Autowired
+    public UserService(IUserRepository userRepository, ModelMapper modelMapper) {
+        _userRepository = userRepository;
+        _modelMapper = modelMapper;
+    }
 
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        return null;
+        User user = _modelMapper.map(userDTO, User.class);
+        return _modelMapper.map(_userRepository.save(user), UserDTO.class);
     }
 
     @Override
