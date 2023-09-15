33567ef74adcc4775d7972aae0dd4861193df525
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-15 20:29:06 -0300

    feat: Creating the new response MicroserviceResponse

---
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/domain/model/userMicroservice/UserMicroserviceRole.java
@@ -0,0 +1,31 @@
+package br.com.ms.authandauto.domain.model.userMicroservice;
+
+import br.com.ms.authandauto.domain.model.Enum.Role;
+import br.com.ms.authandauto.domain.model.microsservice.Microservice;
+import br.com.ms.authandauto.domain.model.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserMicroserviceRole {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @ManyToOne
+        @JoinColumn(name = "user_id")
+        private User user;
+        @ManyToOne
+        @JoinColumn(name = "microservice_id")
+        private Microservice microservice;
+        @Enumerated(EnumType.STRING)
+        private Role role;
+
+    }
