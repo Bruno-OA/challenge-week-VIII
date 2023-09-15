e58fbecd64916839f2a7d6dedd44c539d20092a4
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-15 12:29:27 -0300

    feat: Creating the User entity

---
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/domain/model/User/User.java
@@ -0,0 +1,20 @@
+package br.com.ms.authandauto.domain.model.User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "users")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
