9b5f49c83e0fcceec11294b43c25566fd5a92b5b
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-16 20:13:54 -0300

    feat: create findByName in IMicroserviceRepository

--- auth-and-auto/src/main/java/br/com/ms/authandauto/domain/interfaces/IMicroserviceRepository.java
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/domain/interfaces/IMicroserviceRepository.java
@@ -4,6 +4,9 @@ import br.com.ms.authandauto.domain.model.microsservice.Microservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IMicroserviceRepository extends JpaRepository<Microservice, Long> {
+    Optional<Microservice> findByName(String name);
 }
