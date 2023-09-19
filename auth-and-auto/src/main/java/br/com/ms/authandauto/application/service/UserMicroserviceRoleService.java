a3258696ee178dd66d55d6c647ec7ae343417823
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2023-09-19 12:19:47 -0300

    feat: implementing the method findUserByMicroserviceId in UserMicroserviceRoleService

--- auth-and-auto/src/main/java/br/com/ms/authandauto/application/service/UserMicroserviceRoleService.java
+++ auth-and-auto/src/main/java/br/com/ms/authandauto/application/service/UserMicroserviceRoleService.java
@@ -4,12 +4,15 @@ import br.com.ms.authandauto.application.dtos.MicroserviceDTO;
 import br.com.ms.authandauto.application.dtos.UserMicroserviceRoleDTO;
 import br.com.ms.authandauto.application.interfaces.IUserMicroserviceRoleService;
 import br.com.ms.authandauto.domain.interfaces.IUserMicroserviceRoleRepository;
+import br.com.ms.authandauto.domain.model.user.Reponse.UserRoleResponse;
 import br.com.ms.authandauto.domain.model.userMicroserviceRole.UserMicroserviceRole;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class UserMicroserviceRoleService implements IUserMicroserviceRoleService {
@@ -40,6 +43,20 @@ public class UserMicroserviceRoleService implements IUserMicroserviceRoleService
         microserviceDTO.setId(microserviceId);
         microserviceDTO.setName(name);
         return microserviceDTO;
+    }
+
+    @Override
+    public List<UserRoleResponse> findUserByMicroserviceId(Long microserviceId) {
+        List<UserMicroserviceRole> userMicroserviceRoles = _userMicroserviceRoleRepository.findUserByMicroserviceId(microserviceId);
+
+        return userMicroserviceRoles.stream()
+                .map(userMicroserviceRole -> {
+                    UserRoleResponse response = new UserRoleResponse();
+                    response.setName(userMicroserviceRole.getUser().getName());
+                    response.setRole(userMicroserviceRole.getRole().toString());
+                    return response;
+                })
+                .collect(toList());
     }
 
 }
