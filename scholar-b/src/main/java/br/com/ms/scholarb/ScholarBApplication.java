63f27faf8b329ab95bc8162ca9dfdd230d2bebe8
Bruno-OA <brunooliveiraalves78@gmail.com>
2023-09-19 14:37:04 -0300

    feat: create ms_b

---
+++ scholar-b/src/main/java/br/com/ms/scholarb/ScholarBApplication.java
@@ -0,0 +1,13 @@
+package br.com.ms.scholarb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarBApplication.class, args);
+	}
+
+}
