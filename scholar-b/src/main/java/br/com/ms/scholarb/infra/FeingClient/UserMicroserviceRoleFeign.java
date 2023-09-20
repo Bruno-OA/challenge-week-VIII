bbf1b4187b9eb70756a1779bd1720a244f849819
Bruno-OA <brunooliveiraalves78@gmail.com>
2023-09-20 16:05:53 -0300

    fix: url

--- scholar-b/src/main/java/br/com/ms/scholarb/infra/FeingClient/UserMicroserviceRoleFeign.java
+++ scholar-b/src/main/java/br/com/ms/scholarb/infra/FeingClient/UserMicroserviceRoleFeign.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
-@FeignClient(name = "ms-auth-and-auth", url = "http://localhost:8080/ms-auth-and-auto")
+@FeignClient(name = "ms-auth-and-auth", url = "${client.post.baseUrl}")
 public interface UserMicroserviceRoleFeign {
 
     @GetMapping(value = "/api/userMicroserviceRole/user/{id-user}/microservice/{id-microservice}")
